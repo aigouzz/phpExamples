4f247d8bb2f2988968210de0ae25682d4ab16688
gxc <1003823149@qq.com>
2015-08-01 17:23:02 +0800

    add date function and mktime

--- eof_file.php
+++ eof_file.php
@@ -13,9 +13,10 @@ fclose($fh);
 ?>
 <?php
 //fgetss($fh,1024,$tags);从所有输入中清除所有html和php标签
+date_default_timezone_set("Asia/Shanghai");
 $time = time();
 $mktime = mktime(12,05,16,8,1,2015);
-echo $time.'/'.'make time:'.date("F d,Y h:i:s",$mktime);
+echo date('Y-m-d h:i:sa',$time).'/'.'make time:'.date("m d,Y h:i:sa",$mktime);
 
 
 
