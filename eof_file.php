10b850191ee7582fd7be67d68795bc55a10b5b6d
gxc <1003823149@qq.com>
2015-08-01 12:11:56 +0800

    add date function and mktime

--- eof_file.php
+++ eof_file.php
@@ -13,8 +13,9 @@ fclose($fh);
 ?>
 <?php
 //fgetss($fh,1024,$tags);从所有输入中清除所有html和php标签
-
-
+$time = time();
+$mktime = mktime(12,05,16,8,1,2015);
+echo $time.'/'.'make time:'.date("F d,Y h:i:s",$mktime);
 
 
 
