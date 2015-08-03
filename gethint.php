e934342e939fb4e4cc57d6fc0869c921ae98c1fc
gxc <1003823149@qq.com>
2015-08-03 19:11:45 +0800

    gethint changes

--- gethint.php
+++ gethint.php
@@ -2,25 +2,21 @@
 /**
  * Created by PhpStorm.
  * User: tuyoo
- * Date: 15/7/22
- * Time: 上午10:15
+ * Date: 15/8/3
+ * Time: 下午4:38
  */
 $q = $_GET['q'];
-
 if($q != ''){
     $servername = 'localhost';
     $username = 'root';
     $password = '';
     $dbname = 'my_db';
-
     $con = new mysqli($servername,$username,$password,$dbname);
     if($con->connect_error){
         die('mysql error:'.$con->connect_errno);
     }
-
     $sql = 'select * from Persons where id="'.$q.'"';
     $result = $con->query($sql);
-
     if($result->num_rows > 0){
         while($row = $result->fetch_assoc()){
             echo 'id:'.$row['id'].'<br />firstname:'.$row['Firstname'].'<br />lastname:'.$row['Lastname'].'<br />Age:'.$row['Age'].'<br/>'.$row['Date'];
@@ -28,3 +24,10 @@ if($q != ''){
     }
     $con->close();
 }
+
+?>
+
+
+
+
+
