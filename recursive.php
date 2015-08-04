d4751d63c9db8b8c28b9ff175534698b988bafdb
gxc <1003823149@qq.com>
2015-08-04 15:57:51 +0800

    algorithm upload

--- recursive.php
+++ recursive.php
@@ -36,8 +36,9 @@ function binarySearch($arr,$k){
 //数组必须先排序在查找
 
 //选择排序：从第一个元素开始找到最小的与第一个交换，在剩下的元素中寻找到最小与第二个交换，recursive
-//插入排序：
-/*function switchOrder($arr,$j,$m){
+//插入排序：从第二个元素和前面已经排序的元素对比，如果小于前面元素则兑换顺序，直到大于或等于前面元素停止
+//插入排序涉及比较比选择排序平均少一半！
+function switchOrder($arr,$j,$m){
     $n = $arr[$j];
     $arr[$j] = $arr[$m];
     $arr[$m] = $n;
@@ -53,14 +54,33 @@ function  sort($arr){
             }
         }
     }
-}*/
+}
 
 
 $arr = [1,4,5,8,12,3,63,21,42,33,28,1,5,7];
 
-/*sort($arr);*/
+sort($arr);
 echo $arr;
 
-
+//希尔排序：插入排序  多走几步
+function sortHill($arr){
+    $n = count($arr);
+    $h = 1;
+    while($h<$n/3){
+        $h = $h*3 +1;
+    }
+    while($h>=1) {
+        for ($i = 0; $i < $n; $i++) {
+            for ($j = $i; $j > 0; $j -= $h) {
+                if ($arr[$j] < $arr[$j - $h]) {
+                    switchOrder($arr, $j, $j - $h);
+                } else {
+                    break;
+                }
+            }
+        }
+        $h = $h/3;
+    }
+}
 
 
