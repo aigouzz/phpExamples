abdfc1ce15881071135288655a98471ebc4cdbf6
gxc <1003823149@qq.com>
2015-08-12 17:05:55 +0800

    php with html

---
+++ withHtml.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tuyoo
+ * Date: 15/8/12
+ * Time: 下午5:04
+ */
+$name = 'mysql';
+$age = 18;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo $name;
+?>
+</body>
+</html>
