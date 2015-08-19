7bad4041d4075f2f54f69eb051a4c3927f27cd16
gxc <1003823149@qq.com>
2015-08-19 18:59:07 +0800

    php upload

--- upload.php
+++ upload.php
@@ -6,11 +6,11 @@
  * Time: 下午4:28
  */
 $pic = $_POST['picture'];
-
-if(is_uploaded_file($_FILES['picture']['tmp_name'])){
+var_dump($pic);
+/*if(is_uploaded_file($_FILES['picture']['tmp_name'])){
     $pic1 = $_FILES['picture']['name'];
 
     echo 'name:'.$pic1.'<br />'.'place:'.$_FILES['picture']['tmp_name'];
-}
+}*/
 
 
