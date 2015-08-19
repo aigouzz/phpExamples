876f84f483d5b112040ea08a20ecc25f7cb4b7a9
gxc <1003823149@qq.com>
2015-08-19 18:57:04 +0800

    insert add

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@
  * Date: 15/8/8
  * Time: 上午11:15
  */
-$servername = 'localhost';
+/*$servername = 'localhost';
 $username = 'root';
 $password = '123456';
 $dbname = 'my_db';
@@ -14,7 +14,25 @@ $con = new mysqli($servername,$username,$password);
 if($con->connect_error){
     die('mysql error:'.$con->connect_errno);
 }
-$sql = 'create ';
+$sql = 'create ';*/
+$a = true;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
 
+</body>
+<script>
+    if('<?php echo $a;?>'){
+        alert(1);
+    }else{
+        alert(2);
+    }
+</script>
+</html>
 
 
