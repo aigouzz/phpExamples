b10b2fd185dc757262d58c168089a3d006fbbd36
gxc <1003823149@qq.com>
2015-08-22 09:42:13 +0800

    upload to git

--- upload.php
+++ upload.php
@@ -5,9 +5,13 @@
  * Date: 15/8/3
  * Time: 下午4:28
  */
-$pic = $_POST['picture'];
-$pi = $_GET['a'];
-var_dump($pi);
+//$pic = $_POST['picture'];
+$pi = $_REQUEST['a'];
+if(!empty($pi)){
+    echo '1';
+}else{
+    echo '0';
+};
 /*if(is_uploaded_file($_FILES['picture']['tmp_name'])){
     $pic1 = $_FILES['picture']['name'];
 
