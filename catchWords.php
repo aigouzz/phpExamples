02adc02da1a584584d8d3ec48ec400c89573c324
gxc <1003823149@qq.com>
2015-08-26 18:24:52 +0800

    catch word with new way

--- catchWords.php
+++ catchWords.php
@@ -7,11 +7,10 @@
  */
 $address = "http://doudizhu.tuyoo.com";
 $content = file_get_contents($address);
-preg_match("/(<ul\sid=\"lfUl\">(\s*<li>\s*<a[\s\S]*<\/a>\s*<\/li>\s*){5}<\/ul>)/i",$content,$game);
+preg_match("/<ul\sid=\"lfUl\">[\s\S]{100,2000}<\/ul>/i",$content,$game);
 preg_match("/(<ul[\s\S]*>)/i",$content,$ul);
-var_dump($game);
 //var_dump($ul[0]);
-var_dump($content);
+//var_dump($content);
 
 /*preg_match("/http:\/\/www\.[\s\S]*\.com\//i",$content,$a1);
 preg_match("/get\bsome\brest/i",$content,$a2);
@@ -23,21 +22,42 @@ preg_match("//i",$content,$a2);*/
 
 /*正则表达式之挖掘文本内容
  * 聚类
- * kvm
- * k-means
+ * svm:support vector machine
+ * k-means:algorithm
  *
+ *
+ * */
+/*五大算法：
+ * 分治算法
+ * 动态规划算法
+ * 贪心算法
+ * 回溯法
+ * 分支限界法
+ *
+ * 递推回溯
+ * 递归
  *
  * */
 
 
-
-
-
-
-
-
-
-
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>3g.tuyoo.com</title>
+    <link href="http://www.tuyoo.com/statics/css/tuyoo/main.css" rel="stylesheet" />
+    <style>
+        #wrap{width:639px;margin:0 auto;}
+        #lfUl{background: #137b2d;}
+    </style>
+</head>
+<body>
+<div id="wrap">
+    <?php echo $game[0];?>
+</div>
+</body>
+</html>
 
 
 
