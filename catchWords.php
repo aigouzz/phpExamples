a821346d1b81062dcb13c7c43a123a59c5fe772b
gxc <1003823149@qq.com>
2015-08-26 10:41:00 +0800

    add datamining

--- catchWords.php
+++ catchWords.php
@@ -7,10 +7,40 @@
  */
 $address = "http://doudizhu.tuyoo.com";
 $content = file_get_contents($address);
-preg_match("/(<ul\sid=\"lfUl\">[\s\S]*<\/ul>)/i",$content,$game);
+preg_match("/(<ul\sid=\"lfUl\">(\s*<li>[\s\S]*<\/li>\s*){5}\s*<\/ul>)/i",$content,$game);
 preg_match("/(<ul[\s\S]*>)/i",$content,$ul);
-var_dump($game[0]);
-var_dump($ul[0]);
+var_dump($game);
+//var_dump($ul[0]);
 var_dump($content);
+
+/*preg_match("/http:\/\/www\.[\s\S]*\.com\//i",$content,$a1);
+preg_match("/get\bsome\brest/i",$content,$a2);
+
+var_dump($a1);
+var_dump($a2);
+
+preg_match("//i",$content,$a2);*/
+
+/*正则表达式之挖掘文本内容
+ * 聚类
+ * kvm
+ * k-means
+ *
+ *
+ * */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
