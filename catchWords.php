b46f8aec06847e79ff4cc11a65ba63f8e04bfe9e
gxc <1003823149@qq.com>
2015-08-26 08:50:01 +0800

    catchword add to git

--- catchWords.php
+++ catchWords.php
@@ -5,3 +5,12 @@
  * Date: 15/8/25
  * Time: 下午4:38
  */
+$address = "http://doudizhu.tuyoo.com";
+$content = file_get_contents($address);
+preg_match("/(<ul\sid=\"lfUl\">[\s\S]*<\/ul>)/i",$content,$game);
+preg_match("/(ul)/i",$content,$ul);
+var_dump($game[0]);
+var_dump($ul);
+var_dump($content);
+
+
